c71f737e389ccbfba435baf24bdfe2a3a0885fd0
Rimjhim <kumaririmjhim720@gmail.com>
2022-06-05 13:30:12 +0530

    commit

--- PascalTriangle/Solution.java
+++ PascalTriangle/Solution.java
@@ -1,7 +1,6 @@
 package PascalTriangle;
 
 import java.util.* ;
-import java.io.*; 
 import java.util.ArrayList;
 public class Solution {
     public List<List<Integer>> generate(int numRows) {
@@ -21,3 +20,27 @@ public class Solution {
     
     }
 }
+
+
+
+//codeStudio 
+
+// public class Solution {
+// 	public static ArrayList<ArrayList<Long>> printPascal(int n) {
+//                  // Write your code here.
+//          ArrayList<ArrayList<Long>> res = new ArrayList<ArrayList<Long>>();
+//         ArrayList<Long> row, pre = null;
+//        for(int i=0;i<n;i++){
+//          row = new ArrayList<>();
+//     for(int j=0;j<=i;j++){
+//                  if(j==0|| j==i)
+//                       row.add(1L);
+//                      else
+//                       row.add(pre.get(j-1)+pre.get(j));
+//                 }
+//            pre = row;
+//             res.add(row);
+//        }
+//        return res;
+// 	}
+// }
